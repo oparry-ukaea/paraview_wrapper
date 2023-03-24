b26c45e2d7b60d670045e177b2b0ed00fa2f1435
Owen Parry <owen.parry@ukaea.uk>
2023-03-24 15:41:47 +0000

    Runner template

---
+++ runner.template.py
@@ -0,0 +1,8 @@
+from NESO import plot_fluid_particle_img
+from utils import get_output_dir
+
+plot_fluid_particle_img(
+    data_dir="[NOT_SET]",
+    output_dir=get_output_dir(),
+    output_time=39.0,
+)
