d6aec040480af8a08060be63c2f7f6a26fd290b0
Owen Parry <owen.parry@ukaea.uk>
2023-08-11 17:12:43 +0100

    Util function to get Desktop path.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
 from .locations import get_output_dir, get_output_fpath
+from .system import get_desktop_dir
---
+++ utils/system.py
@@ -0,0 +1,11 @@
+import os
+
+
+def get_desktop_dir():
+    loc = ""
+    if "HOME" in os.environ:
+        loc = os.path.join(os.environ["HOME"], "Desktop")
+    if loc and os.path.isdir(loc):
+        return loc
+    else:
+        raise (RuntimeError("get_desktop_dir: Desktop dir not found"))
