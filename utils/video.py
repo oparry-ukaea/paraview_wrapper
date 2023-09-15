9d781f7c4c735a057d7fc88048f154e9c62817b1
Owen Parry <owen.parry@ukaea.uk>
2023-09-15 15:02:38 +0100

    Overwrite output by default in avi_to_mp4 util.

--- utils/video.py
+++ utils/video.py
@@ -2,7 +2,14 @@ import ffmpeg
 import os.path
 
 
-def avi_to_mp4(common_dir, fbase, fpath_in="", fpath_out="", ffmpeg_output_options={}):
+def avi_to_mp4(
+    common_dir,
+    fbase,
+    fpath_in="",
+    fpath_out="",
+    overwrite_output=True,
+    ffmpeg_output_options={},
+):
     if not fpath_in:
         fpath_in = os.path.join(common_dir, fbase + ".avi")
     if not fpath_out:
@@ -13,4 +20,4 @@ def avi_to_mp4(common_dir, fbase, fpath_in="", fpath_out="", ffmpeg_output_optio
     ffmpeg_output_options_int.update(ffmpeg_output_options)
     ffmpeg.input(fpath_in, f="avi").output(
         fpath_out, f="mp4", **ffmpeg_output_options_int
-    ).run()
+    ).run(overwrite_output=overwrite_output)
