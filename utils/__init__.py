b920f6493759c143e352868b2a06ccabd157f389
Owen Parry <owen.parry@ukaea.uk>
2023-10-27 11:39:28 +0100

    Create function to locate paraview python site-package(s).

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .locations import get_output_dir, get_output_fpath
-from .system import get_desktop_dir
+from .system import find_paraview, get_desktop_dir
 from .video import avi_to_mp4
--- utils/system.py
+++ utils/system.py
@@ -1,4 +1,6 @@
+import glob
 import os
+import sys
 
 
 def get_desktop_dir():
@@ -9,3 +11,31 @@ def get_desktop_dir():
         return loc
     else:
         raise (RuntimeError("get_desktop_dir: Desktop dir not found"))
+
+
+def _paraview_importable():
+    try:
+        import paraview
+
+        return True
+    except:
+        return False
+
+
+def find_paraview():
+    if _paraview_importable():
+        return
+    else:
+        # Try and find via environment variable
+        site_packages_from_envvar = glob.glob(
+            os.path.join(os.getenv("PARAVIEW_ROOT"), "lib/python*/site-packages")
+        )
+        if site_packages_from_envvar:
+            sys.path.append(site_packages_from_envvar[0])
+
+        if _paraview_importable():
+            return
+        else:
+            raise ModuleNotFoundError(
+                "Unable to find paraview - add <paraview>/lib/python*/site-packages to your PYTHONPATH or define PARAVIEW_ROOT env var."
+            )
