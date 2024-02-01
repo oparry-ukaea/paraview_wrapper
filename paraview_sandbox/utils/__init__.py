71830fe94d6f980fee9b033a1c9ce4ea10c398d0
Owen Parry <owen.parry@ukaea.uk>
2024-02-01 09:54:14 +0000

    Add utility function to read vtu data that works on local and remote hosts.

--- paraview_sandbox/utils/__init__.py
+++ paraview_sandbox/utils/__init__.py
@@ -2,5 +2,5 @@ from .locations import get_output_dir, get_output_fpath
 from .nektar import get_nektar_params
 from .plotting import get_color_array
 from .pv import gen_opacity_pts
-from .system import get_desktop_dir
+from .system import get_desktop_dir, get_vtu_data
 from .video import avi_to_mp4
--- paraview_sandbox/utils/system.py
+++ paraview_sandbox/utils/system.py
@@ -1,5 +1,10 @@
+import datetime
 import glob
 import os
+import os.path
+from paraview.simple import XMLUnstructuredGridReader
+import paraview.util
+import re
 import sys
 
 
@@ -11,3 +16,35 @@ def get_desktop_dir():
         return loc
     else:
         raise (RuntimeError("get_desktop_dir: Desktop dir not found"))
+
+
+def get_vtu_data(
+    data_dir,
+    vtu_basename="",
+    registration_name=None,
+):
+    # Default registration name
+    if registration_name is None:
+        registration_name = "vtu_data" + datetime.datetime.now().strftime(
+            "%Y-%m-%d-%H-%M-%S-%f"
+        )
+
+    glob_pattern = f"{data_dir}/{vtu_basename}*.vtu"
+    vtu_fpaths = paraview.util.Glob(path=glob_pattern)
+
+    # Sort
+    pattern = re.compile(r".*_([0-9]*).vtu")
+    vtu_fpaths = sorted(vtu_fpaths, key=lambda s: int(pattern.search(s).groups()[0]))
+
+    # Check for multiple basenames if none was specified
+    if not vtu_basename:
+        unique_basenames = set([os.path.basename(p) for p in vtu_fpaths])
+        if len(unique_basenames) > 1:
+            print(
+                f"get_vtu_data: WARNING - Found vtus with multiple basenames in {data_dir}; pass 'vtu_basename' to choose one"
+            )
+
+    vtu_data = XMLUnstructuredGridReader(
+        registrationName=registration_name, FileName=vtu_fpaths
+    )
+    return vtu_data
