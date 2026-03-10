a03d61fa7f0319d233fa8c67d1fff0e7be6d5948
Owen Parry <owen.parry@ukaea.uk>
2026-03-10 16:11:49 +0000

    Add make_dir_on_desktop util.

--- paraview_wrapper/utils/system.py
+++ paraview_wrapper/utils/system.py
@@ -1,6 +1,7 @@
 import os
 import os.path
 
+
 def get_desktop_dir():
     loc = ""
     if "HOME" in os.environ:
@@ -9,3 +10,11 @@ def get_desktop_dir():
         return loc
     else:
         raise (RuntimeError("get_desktop_dir: Desktop dir not found"))
+
+
+def make_dir_on_desktop(dirname):
+    desktop_dir = get_desktop_dir()
+    new_dir = os.path.join(desktop_dir, dirname)
+    if not os.path.isdir(new_dir):
+        os.makedirs(new_dir)
+    return new_dir
